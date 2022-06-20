3bb5b5c41988e9ad95af1ffe36db754045594096
Mohamed Ali marouane <mohamedali.marouane7@gmail.com>
2022-06-21 01:24:25 +0200

    learn props

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import Profile from './profile/profile.js'
+import image from './profile/pic.jpg'
+
 
 function App() {
+  const fullName = "Mohamed Ali Marouane"
+  const bio = "Management student  "
+  const profession= "Student"
+  const handlName =(fullName) => alert (`My NAME is ${fullName}`)
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1> React props checkpoint</h1>
+      <Profile name={fullName} my_bio={bio} my_profession={profession}  handlName={handlName}>
+      <img className='taswira' src={image} alt="profilpic" />
+      </Profile>
       </header>
     </div>
   );
 }
+
 
 export default App;
